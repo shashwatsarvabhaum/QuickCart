6ab8077af1bb3707e3f09fc1ca831c04a17e9565
Shashwat Sarvabhaum <shashwatsarvabhaum.6490@gmail.com>
2025-07-14 19:39:42 +0530

    Fix: Added missing imports for syncUserUpdate and syncUserDeletion

--- app/api/inggest/route.js
+++ app/api/inggest/route.js
@@ -1,12 +1,16 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation } from "@/config/inngest";
-
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
+import {
+  inngest,
   syncUserCreation,
   syncUserUpdate,
   syncUserDeletion
+} from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserDeletion
   ],
 });
