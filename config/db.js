a48f3a3c8a6c9dd599b6c8d77c7f9b7d5665ec34
Shashwat Sarvabhaum <shashwatsarvabhaum.6490@gmail.com>
2025-07-14 16:52:57 +0530

    refactor: clean up db.js and update package.json with gh-pages dependency

--- config/db.js
+++ config/db.js
@@ -1,48 +1,24 @@
-import mongoose
+import mongoose from 'mongoose';
+import { MONGODB_URI } from './env.js';
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
-    }
+    cached = global.mongoose = { conn: null, promise: null };
+}
 
-    async function dbConnect() {
-        if (cached.conn) {
-            return cached.conn
-        }
-
-        if (!cached.promise) {
-            cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-                return mongoose
-            })
-        }
-
-        cached.conn = await cached.promise
-        return cached.conn
-    }
-    from 'mongoose'
-import { MONGODB_URI } from './env.js'
-export default dbConnect    
-import { MONGODB_URI } from './env.js'
-import mongoose from 'mongoose'
-export { dbConnect }
-import { MONGODB_URI } from './env.js'
-import mongoose from 'mongoose'
 export default async function dbConnect() {
     if (cached.conn) {
-        return cached.conn
+        return cached.conn;
     }
 
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-            return mongoose
-        })
+            return mongoose;
+        });
     }
 
-    cached.conn = await cached.promise
-    return cached.conn
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
-import mongoose from 'mongoose'
-import { MONGODB_URI } from './env.js'
-import { MONGODB_URI } from './env.js'
 
