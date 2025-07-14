628389862bf21f4c07f99caaab639725838b8936
Shashwat Sarvabhaum <shashwatsarvabhaum.6490@gmail.com>
2025-07-14 18:00:56 +0530

    fix: converted db.js to CommonJS

--- config/db.js
+++ config/db.js
@@ -4,21 +4,24 @@ import { MONGODB_URI } from './env.js';
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default async function dbConnect() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-            return mongoose;
-        });
-    }
-
-    cached.conn = await cached.promise;
+async function dbConnect() {
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const MONGODB_URI = process.env.MONGODB_URI;
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
+module.exports = dbConnect;
